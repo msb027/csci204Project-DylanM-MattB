339752a46205e8b863c05c25fd624be4b6d5a84b
msb027 <msb027@bucknell.edu>
2017-03-21 14:24:27 -0400

    Add files via upload

--- src/plot.py
+++ src/plot.py
@@ -1,4 +1,6 @@
 """
+Matt Brown
+Dylan Mendelowitz
 This file contains objects related to our plotting
 """
 
@@ -6,6 +8,8 @@ This file contains objects related to our plotting
 import matplotlib
 matplotlib.use("TkAgg")
 import matplotlib.pyplot as plt
+import numpy as np
+from mpl_toolkits.axes_grid1 import make_axes_locatable
 
 
 class MyPlot:
@@ -24,12 +28,33 @@ class MyPlot:
 
     @staticmethod
     def twoDScatter(xList, yList, xLabels=None, yLabels=None):
-        pass
+        '''
+        wrapper for creating a 2D scatter plot
+        '''
+        x = xList
+        y = yList
+        plt.scatter(x,y)
+        plt.xticks = xLabels
+        plt.yticks = yLabels
+        plt.show
 
 
     @staticmethod
     def twoDBar(xList, yList, xLabels=None, yLabels=None):
-        pass
+        '''
+        wrapper to create a 2D bar graph
+        '''
+        n_groups = len(xList)
+        index = np.arange(n_groups)
+        bar_width = 0.1
+
+        opacity = 0.8
+
+        rects1 = plt.bar(index/2, max(yList) +10 , bar_width,\
+                         alpha=opacity,\
+                         color='g')
+        plt.yticks(yLabels)
+        plt.xticks(xLabels)
 
 
 
