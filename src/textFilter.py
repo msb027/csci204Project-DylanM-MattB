d88ea21e09a80a4b24751710b7964e0db5766c83
dylanmendelowitz <dmm053@bucknell.edu>
2017-04-04 14:51:47 -0400

    Add files via upload

--- src/textFilter.py
+++ src/textFilter.py
@@ -11,7 +11,6 @@ class TextFilter:
     """
     Applies a list of filter to a document and returns same filtered Document
     """
-
     def __init__(self, filterList=None, doc=None):
         self.__filterList = filterList
         self.__doc = doc
@@ -22,28 +21,50 @@ class TextFilter:
     def setDoc(self, doc):
         self.__doc = doc
 
-    def appy(self, doc=None):
+    def apply(self, doc=None):
         """
         doc is the Document we are applying each filter in the filterlist to
         """
-        pass
+        if doc != None:
+            self.setDoc(doc)
+        for aFilter in self.__filterList:
+            if aFilter == "normalizeWhiteSpace": self.normalizeWhiteSpace()
+            elif aFilter == "normalizeCase": self.normalizeCase()
+            elif aFilter == "stripNull": self.stripNull()
+            elif aFilter == "stripNumbers": self.stripNumbers()
+            elif aFilter == "stripFiles": self.stripFiles()
 
     def normalizeWhiteSpace(self):
-        pass
+        for i in range(self.__doc.getSCount()):
+            temp = self._doc[i].split()
+            temp = " ".join(temp)
+            self._doc[i] = temp
 
     def normalizeCase(self):
-        pass
+        for i in range(self.__doc.getSCount()):
+            self._doc[i] = self._doc[i].lower()
     
     def stripNull(self):
-        pass
+        toRemove = []
+        for i in range(self._doc.getSCount()):
+            for ch in self._doc[i]:
+                if not ch.isdigit() and not ch.isalpha():
+                    toRemove.append(ch)
+            for ch in toRemove:
+                self._doc[i] = self._doc[i].replace(ch,"")
 
     def stripNumbers(self):
-        pass
-
+        for i in range(self.__doc.getSCount()):
+            for num in range(10):
+                self._doc[i] = self._doc[i].replace(str(num),"")
+            
     def stripFiles(self):
-        pass
-
-
+        file = open("stripfile.txt","r")
+        words = file.readlines()
+        for i in range(self._doc.getSCount()):
+            for word in words:
+                self._doc[i] = self._doc[i].replace(word,"")
+            
 
 def testTestFilter():
     """
